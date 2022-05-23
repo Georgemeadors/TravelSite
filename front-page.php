4f03fb842285de9e3586a58d210ba412d42e5673
George Meadors <gmeadors04@gmail.com>
2022-05-23 12:22:35 -0700

    completed nav bar added homepage

---
+++ front-page.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Template Name: Homepage *
+* @package TravelSite
+*
+*/
+?>
+<?php get_header(); ?>
+ <!-- This is the start of hero section -->
+ <main id="topBtn">
+        <div class="hero-text">
+            <h1>Hello World</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    </main>
+    <!-- This is the end of hero section --> 
